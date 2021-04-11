cfd1f556e845e8b4eb1e25ecce4d1c1e0e0049c1
Kyle Fenole <kfenole@gmail.com>
2021-04-10 17:42:42 -0700

    Minor Improvements

--- bot.py
+++ bot.py
@@ -201,7 +201,7 @@ async def on_message(message):
         print('Sending the tragedy')
         await message.channel.send('Did you ever hear the Tragedy of Darth Plagueis the wise? I thought not. It\'s not a story the Jedi would tell you. It\'s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. It\'s ironic he could save others from death, but not himself.')
 
-    if 'minecraft' in message.content.lower() or '-ip' in message.content.lower():
+    if '-minecraft' in message.content.lower() or '-ip' in message.content.lower()or 'minecraft ip' in message.content.lower() or 'server ip' in message.content.lower():
         print('Sending the minecraft info')
         await message.channel.send('Our Minecraft Server (Java, MC 1.15.2): **142.44.222.25:25726**')
 
@@ -455,7 +455,7 @@ async def on_message(message):
         today = date.today()
         d = today.strftime("%A")
         advent = False
-        lent = True
+        lent = False
         msg = ''
         if (d == 'Monday' or d == 'Saturday' or (advent and d == 'Sunday')):
             msg += '**The Joyful Mysteries** (' + d + ')\n'
